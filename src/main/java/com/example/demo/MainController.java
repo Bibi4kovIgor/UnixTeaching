4e1a669d5f1916fb9ea4915c86cd484a684b89d1
Ihor Bibichkov <work.bibi4kov@gmail.com>
2021-03-05 13:49:12 +0200

    Minor changes 2

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
 
         @GetMapping("/")
         public String index() {
-            return "Hello, Guys!";
+            return "Hello, super guys!";
         }
 
     }
